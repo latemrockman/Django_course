8927f5b4f7a676c586f91b63c5a3d7ba457c3116
Andrew <andrew.am2017@gmail.com>
2022-05-07 20:55:32 +0300

    36 Изменение и удаление записей

--- movie_project/movie_app/models.py
+++ movie_project/movie_app/models.py
@@ -4,7 +4,14 @@ from django.db import models
 
 class Movie(models.Model):
     name = models.CharField(max_length=40)
-    rating = models. IntegerField()
+    rating = models.IntegerField()
+    year = models.IntegerField(null=True)
+    budget = models.IntegerField(default=10000000)
+
+
 
     def __str__(self):
         return f'{self.name} - {self.rating}%'
+
+
+# from movie_app.models import Movie
