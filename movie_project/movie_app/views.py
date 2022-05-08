2972121baebf5ff206051cebf6a0eff0b314a900
Andrew <andrew.am2017@gmail.com>
2022-05-08 21:34:51 +0300

    # 39 Выводим QuerySet в шаблон

--- movie_project/movie_app/views.py
+++ movie_project/movie_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Movie
+
+
+def show_all_movie(request):
+    movies = Movie.objects.all()
+    return render(request, 'movie_app/all_movies.html', {'movies': movies})
