6e959072efc6308da3a83768ff587346805a8b89
Andrew <andrew.am2017@gmail.com>
2022-05-11 22:40:01 +0300

    # 43 функции агрегации

--- movie_project/movie_app/views.py
+++ movie_project/movie_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from django.db.models import F
+from django.db.models import F, Sum, Max, Min, Count, Avg
 
 # Create your views here.
 from .models import Movie
@@ -7,10 +7,16 @@ from .models import Movie
 
 def show_all_movie(request):
     movies = Movie.objects.order_by(F('year').desc(nulls_last=True), '-rating')
+    agg = movies.aggregate(Avg('budget'), Max('rating'), Min('rating'), Count('name'))
 
     for movie in movies:
         movie.save()
-    return render(request, 'movie_app/all_movies.html', {'movies': movies})
+    return render(request, 'movie_app/all_movies.html',
+                  {'movies': movies,
+                   'agg': agg,
+                   'total': movies.count(),                         # кол-во записей можно еще так получить
+                   'midl': movies.aggregate(Avg('budget'))          # среднее кол-во так
+                   })
 
 def show_one_movie(request, slug_movie:str):
     movie = get_object_or_404(Movie, slug=slug_movie)
