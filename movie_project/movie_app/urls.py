003c67f82a4e486116bde77ea849121a043e2bdd
Andrew <andrew.am2017@gmail.com>
2022-05-12 20:01:46 +0300

    # 45 Вычисляемые поля с annotate

--- movie_project/movie_app/urls.py
+++ movie_project/movie_app/urls.py
@@ -2,6 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.show_all_movie),
+    path('', views.show_all_movie, name='movie-index'),
     path('movie/<slug:slug_movie>', views.show_one_movie, name="movie-detail"),
 ]
--- movie_project/movie_app/views.py
+++ movie_project/movie_app/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render, get_object_or_404
-from django.db.models import F, Sum, Max, Min, Count, Avg
+from django.db.models import F, Sum, Max, Min, Count, Avg, Value
 
 # Create your views here.
 from .models import Movie
 
 
 def show_all_movie(request):
-    movies = Movie.objects.order_by(F('year').desc(nulls_last=True), '-rating')
+    #movies = Movie.objects.order_by(F('year').desc(nulls_last=True), '-rating')
+
+    movies = Movie.objects.annotate(
+        privet_true=Value(True),
+        privet_false=Value(False),
+        privet_str=Value('hello'),
+        privet_int=Value(123),
+        new_budget=F('budget') + 555,
+        new_rating=F('rating') * F('year')
+    )
+
     agg = movies.aggregate(Avg('budget'), Max('rating'), Min('rating'), Count('name'))
 
     for movie in movies:
