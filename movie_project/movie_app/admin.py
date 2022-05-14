d27c28026a4f2a5a2e0ec24ebe162d3f9e4c78a0
Andrew <andrew.am2017@gmail.com>
2022-05-14 22:36:59 +0300

    # 50 Вычисляемое поле

--- movie_project/movie_app/admin.py
+++ movie_project/movie_app/admin.py
@@ -5,10 +5,19 @@ from .models import Movie
 # Register your models here.
 
 
-@admin.register(Movie)                                          # за классом админ указываем клас MovieAdmin
-class MovieAdmin(admin.ModelAdmin):                             # класс обычно называют по названию модели + Admin
-    list_display = ['name', 'rating', 'year',  'budget']        # перечисляем строками поля из класса Movie, 0й лемент будет ссылкой
-    list_editable = ['rating', 'year']                          # перечисляем поля, которые можно редактировать из таблицы, поле 'name' нельзя указывать тк оно будет ссылкой
-    ordering = ['rating', '-year']                              # сортировка, по рейтингу первостепенная, по году второстепенная
-    list_per_page = 3                                           # сколько записей отображается на 1й странице
+@admin.register(Movie)                                                      # за классом админ указываем клас MovieAdmin
+class MovieAdmin(admin.ModelAdmin):                                         # класс обычно называют по названию модели + Admin
+    list_display = ['name', 'rating', 'year',  'budget', 'rating_status']   # перечисляем строками поля из класса Movie, 0й лемент будет ссылкой
+    list_editable = ['rating', 'year']                                      # перечисляем поля, которые можно редактировать из таблицы, поле 'name' нельзя указывать тк оно будет ссылкой
+    ordering = ['rating', '-year']                                          # сортировка, по рейтингу первостепенная, по году второстепенная
+    list_per_page = 15                                                      # сколько записей отображается на 1й странице
 
+    @admin.display(description='Оценка')                                    # Задать название колонки (по умолчанию название берется по названию метода)
+    def rating_status(self, mov: Movie):                                    # rating_status - название колонки, mov - экземпляр класса Movie (название экземпляра может быть любым)
+        if mov.rating < 50:
+            return "Зачем это смотреть?!"
+        if mov.rating < 70:
+            return "Разок можно глянуть..."
+        if mov.rating <= 85:
+            return "Звчет!"
+        return "Топчик!"
