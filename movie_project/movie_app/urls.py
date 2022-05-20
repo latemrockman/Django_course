1e9881799e78396f249928f90e18e66cf1d9755d
Andrew <andrew.am2017@gmail.com>
2022-05-20 17:47:16 +0300

    # Добавил страницу "Список всех режисёров"

--- movie_project/movie_app/urls.py
+++ movie_project/movie_app/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.show_all_movie, name='movie-index'),
-    path('actors/', views.show_all_actors, name='all-actors'),
     path('movie/<slug:slug_movie>', views.show_one_movie, name="movie-detail"),
+    path('actor/', views.show_all_actors, name='all-actors'),
+    path('actor/<int:id_actor>', views.show_one_actor, name='actor-details'),
+    path('director/', views.show_all_directors, name='all-directors'),
+    path('director/<int:id_director>', views.show_one_director, name='director-details')
 ]
--- movie_project/movie_app/views.py
+++ movie_project/movie_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.db.models import F, Sum, Max, Min, Count, Avg, Value
 
 # Create your views here.
-from .models import Movie, Actor
+from .models import Movie, Actor, Director
 
 
 def show_all_movie(request):
@@ -36,3 +36,18 @@ def show_one_movie(request, slug_movie:str):
 def show_all_actors(request):
     actors = Actor.objects.all()
     return render(request, 'movie_app/all_actors.html', {'actors': actors})
+
+
+def show_one_actor(request, id_actor: int):
+    actor = get_object_or_404(Actor, id=id_actor)
+    return render(request, 'movie_app/one_actor.html', {'actor': actor})
+
+
+def show_all_directors(request):
+    director = Director.objects.all()
+    return render(request, 'movie_app/all_directors.html', {'director': director})
+
+
+def show_one_director(request, id_director: int):
+    director = get_object_or_404(Director, id=id_director)
+    return render(request, 'movie_app/one_director.html', {'director': director})
