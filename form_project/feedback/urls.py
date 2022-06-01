61fb73d4fe2f93e9d33f86910f5518eec5551bef
Andrew <andrew.am2017@gmail.com>
2022-06-01 23:34:19 +0300

    # 7.4 Ручная валидация

--- form_project/feedback/urls.py
+++ form_project/feedback/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from .views import index
+from .views import index, done
 
 urlpatterns = [
+    path('done', done, name='done'),
     path('', index, name='index'),
 ]
--- form_project/feedback/views.py
+++ form_project/feedback/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
 
 def index(request):
 
+    if request.method == "POST":
+        name = request.POST['name']
+
+        if len(name) == 0:
+            return render(request, 'feedback/feedback.html', {'got_error': True})
+
+
+        return HttpResponseRedirect('/done')
+
     return render(request, 'feedback/feedback.html', {})
+
+
+def done(request):
+
+    return render(request, 'feedback/done.html', {})
+
 
 
 def hello(request):
